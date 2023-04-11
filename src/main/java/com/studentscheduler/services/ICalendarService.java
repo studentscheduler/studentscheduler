41a4e397f38c7ff9c98ad2f530d1a3040e6e1019
Tara <tarapoudyel999@gmail.com>
2023-04-10 22:13:00 -0400

    Update ICalendarService.java

--- src/main/java/com/studentscheduler/services/ICalendarService.java
+++ src/main/java/com/studentscheduler/services/ICalendarService.java
@@ -1,10 +1,17 @@
 package com.studentscheduler.services;
 
 import com.studentscheduler.dto.CalendarDTO;
+import com.studentscheduler.dto.EventDTO;
 
 public interface ICalendarService {
 	
-	CalendarDTO fetchById(int id);
+	CalendarDTO fetchEvent(int eventId);
+	
+	CalendarDTO createEvent(EventDTO event);
+	
+	CalendarDTO alterEvent(EventDTO event);
+	
+	CalendarDTO deleteEvent(int eventId);
 	
 	void save(CalendarDTO calendarDTO);
 }
